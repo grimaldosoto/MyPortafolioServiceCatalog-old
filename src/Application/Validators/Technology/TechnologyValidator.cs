c4047f6c94efd5598180512da1c8cc56c4235b64
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-07-24 11:50:52 -0500

    Dtos para Technology con FluentValidation

---
+++ src/Application/Validators/Technology/TechnologyValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dtos.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators.Technology
+{
+    public class TechnologyValidator : AbstractValidator<TechnologyRequestDto>
+    {
+        public TechnologyValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("El campo Nombre no puede ser nulo")
+                .NotEmpty().WithMessage("El campo Nombre no puede ser vacío");
+        }
+    }
+}
